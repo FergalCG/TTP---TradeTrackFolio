b02f3e324fd3263cf3325163821e3e9b5809da2f
FergalCG <fergalcg@gmail.com>
2020-02-26 01:21:51 -0500

    feat: Finished Holdings and transactions api routes. Will come back later to secure them.

--- server/api/holdings.js
+++ server/api/holdings.js
@@ -1,9 +1,8 @@
 const router = require('express').Router()
-const Holding = require('../db/models')
+const {Holding} = require('../db/models')
 module.exports = router
 
-router.get('/holdings', async (req, res, next) => {
-    console.log(req.user)
+router.get('/', async (req, res, next) => {
     try {
         const holdings = await Holding.findAll({
             where: {
@@ -11,6 +10,29 @@ router.get('/holdings', async (req, res, next) => {
             }
         })
         res.json(holdings)
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.put('/:ticker', async (req, res, next) => {
+    try {
+        await Holding.update(req.body, {
+            where: {
+                userId: req.user.id,
+                ticker: req.params.ticker
+            }
+        })
+        res.sendStatus(200)
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.post('/', async (req, res, next) => {
+    try {
+        await Holding.create(req.body)
+        res.sendStatus(201)
     } catch (error) {
         next(error)
     }

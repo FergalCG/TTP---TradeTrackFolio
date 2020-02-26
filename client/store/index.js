5752fcee8970efe0da3d0d3e40c4e083d9f90c54
FergalCG <fergalcg@gmail.com>
2020-02-25 19:52:27 -0500

    feat: Transaction and holding reducers, thunks, and action creators set up. Logic for updating holdings efficiently set up.

--- client/store/index.js
+++ client/store/index.js
@@ -3,9 +3,11 @@ import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
+import holding from './holding'
+import transaction from './transaction'
 
 
-const reducer = combineReducers({user})
+const reducer = combineReducers({user, holding, transaction})
 const middleware = composeWithDevTools(
     applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
